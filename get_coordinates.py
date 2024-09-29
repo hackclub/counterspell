0cccfc2c6b9f58eb0742c66aabf70609810acc32
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2024-09-29 00:46:23 -0400

    actually adding map this time

---
+++ get_coordinates.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+# Google Maps Geocoding API key
+API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+
+def get_coordinates(address):
+
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={API_KEY}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        location = data['results'][0]['geometry']['location']
+        return location['lat'], location['lng']
+    
+def process_cities(events_file):
+
+    with open(events_file, 'r') as file:
+        events = json.load(file)
+
+    results = []
+    for event in events:
+        name = event['name']
+        lat, lng = get_coordinates(name)
+        event['latitude'] = lat
+        event['longitude'] = lng
+        results.append(event)
+
+    with open(events_file, 'w') as file:
+        json.dump(results, file, indent=4)
+
+
+events_file = './src/events.json'
+process_cities(events_file)
---
+++ src/components/Map.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react'
+import { Box, Card, Embed } from 'theme-ui'
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Tooltip,
+  useMapEvents
+} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+import events from '../events.json'
+
+const mapIcon = new L.Icon({
+  iconUrl: 'https://assets.hackclub.com/icon-rounded.png',
+  iconRetinaUrl: 'https://assets.hackclub.com/icon-rounded.png',
+  iconAnchor: null,
+  popupAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(20, 20),
+  popupAnchor: [0, 0]
+})
+
+const StyledMapContainer = MapContainer;
+
+export default function Map() {
+  const [center, setCenter] = useState(
+    window.innerWidth > 767.98 ? [35.683, -25.099] : [55, -100]
+  )
+
+  const bounds = [
+    [-85, -Infinity], // Southwest coordinates
+    [85, Infinity]    // Northeast coordinates
+  ];
+
+  return (
+    <>
+      
+      <StyledMapContainer
+        center={center}
+        maxBounds={bounds}
+        maxBoundsViscosity={1.0}
+        zoom={2.5}
+        minZoom={1}
+        style={{ width: '100%', height: '500px' }}
+        worldCopyJump={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {events.map((event, idx) => (
+          <Marker
+            position={[event.latitude, event.longitude]}
+            key={idx}
+            icon={mapIcon}
+          >
+           <Popup>
+            <a href={`https://hackclub.slack.com/archives/${event.slack_id}`} target="_blank" rel="noopener noreferrer">
+              {event.name}
+            </a>
+          </Popup>
+          </Marker>
+        ))}
+      </StyledMapContainer>
+    </>
+  )
+}
