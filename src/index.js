7ad88e3f5a5c03440aff96d684b7e41e76429316
Saahil <saahilattud@gmail.com>
2024-10-02 21:18:28 -0400

    fix: conflicts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./pages/App";
+import NotFound from "./pages/NotFound";
+
+// To add your own page, uncomment the following import statement.
+// Replace `ExampleCity` with the name of your city.
+// E.g. Ottawa, ON -> `Ottawa`.
+// import ExampleCity from "./pages/ExampleCity";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+
+        {/*
+          To add your own page, copy paste and uncomment the following code! 
+          Replace `/example-city` and `ExampleCity` with the name of your city.
+          E.g. Ottawa, ON -> `/ottawa` and `Ottawa`.
+        */}
+        {/* <Route path="/example-city" element={<ExampleCity />} /> */}
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
