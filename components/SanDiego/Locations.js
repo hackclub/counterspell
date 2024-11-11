c84d03be97ec1634c2747b9b84ef2c422949f5b6
Emily Topete <1emilytopete@gmail.com>
2024-11-10 18:54:29 -0800

    Create Locations.js

---
+++ components/SanDiego/Locations.js
@@ -0,0 +1,56 @@
+import Subscribe from "./Subscribe";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("./Map"), { ssr:false });
+
+export default function Locations() {
+  const cities = [
+    "Boston, MA",
+    "New York City, NY",
+    "Ottawa, ON",
+    "Basking Ridge, NJ",
+    "Austin, TX",
+    "San Francisco, CA",
+    "Burlington, VT",
+    "Oshkosh, WI",
+  ];
+
+  return (
+    <div className="relative px-6 py-24 text-center neuebit bg-darker retro">
+      <p className="text-3xl uppercase">It's happening all around you</p>
+      <div className="flex justify-center pt-24">
+        <div className="grid items-center justify-center w-full max-w-5xl grid-cols-1 gap-8 text-2xl lg:grid-cols-2">
+          {cities.map((city, i) => (
+            <p className="lg:odd:text-left lg:even:text-right" key={i}>
+              {city}
+            </p>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-center text-center text-2xl space-y-2 my-20">
+        <div className="border-4 border-dashed border-pink py-6 px-6 neuebit tracking-wider text-4xl max-w-5xl w-full">
+          <Map full={false} />
+        </div>
+      </div>
+
+      <div className="flex justify-center my-20 space-y-2 text-2xl text-center">
+        <div className="px-6 py-6 text-4xl tracking-wider border-4 border-dashed border-pink neuebit">
+          <p>Counterspell is organized by teenagers, for teenagers.</p>
+          <p>
+            See a city near you on the map? {" "}
+            <a
+              href="https://forms.hackclub.com/t/sdMpoL7wK9us"
+              target="_blank"
+              rel="noreferrer"
+              className="text-pink"
+            >
+              Sign up here
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
